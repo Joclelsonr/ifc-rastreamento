48fbd8c9311ec0b46da5fbc36afb892c7fcf5a28
Joclelson Rodrigues <jocleelson.159@hotmail.com>
2024-12-09 11:46:28 -0300

    feat: :sparkles: creation of the endpoint that traces the origin and destination points

--- backend/src/maps/maps.module.ts
+++ backend/src/maps/maps.module.ts
@@ -2,15 +2,18 @@ import { Module } from '@nestjs/common';
 import { PlacesController } from './places/places.controller';
 import { PlacesService } from './places/places.service';
 import { Client as GoogleMapsService } from '@googlemaps/google-maps-services-js';
+import { DirectionsController } from './directions/directions.controller';
+import { DirectionsService } from './directions/directions.service';
 
 @Module({
-  controllers: [PlacesController],
+  controllers: [PlacesController, DirectionsController],
   providers: [
     PlacesService,
     {
       provide: GoogleMapsService,
       useValue: new GoogleMapsService(),
     },
+    DirectionsService,
   ],
 })
 export class MapsModule {}
